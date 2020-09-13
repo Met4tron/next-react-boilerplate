f92aa3279f46ce255974f073a206f28446b574ad
Yuri Gomes <yuurig@gmail.com>
2020-09-13 16:06:28 -0300

    add suport husky, prettier and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 type Props = {
   title: string;
@@ -9,6 +9,7 @@ export default function Home({ title }: Props) {
     <div>
       <Head>
         <title>Create Next App</title>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
